a27103b2e5ed0ea9eb48c9fb981263931da78822
Abel <abelpujol99@gmail.com>
2023-04-20 17:01:24 +0200

    Cube raro

--- physicssimulator-starterpack/code/include/AA4.h
+++ physicssimulator-starterpack/code/include/AA4.h
@@ -10,6 +10,13 @@ private:
 
     graphics::CubePrimitive* _cube;
     glm::mat3 _rotationMatrix;
+    glm::mat3 _angularVelocityMatrix;
+
+    glm::vec3 _angularVelocity;
+
+    float roll = 0.f;
+    float pitch = 0.f;
+    float yaw = 0.f;
 
 public:
 
